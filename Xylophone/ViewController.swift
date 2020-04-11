219d5dbe5fc94a7becc7f5c095b04386ac595056
mikedilulo <mikedilulo2020@gmail.com>
2020-04-11 16:52:35 -0600

    Add Comments for Audio Code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+// Work with audiovisual assets, control device cameras, process audio, and configure system audio interactions.
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //For playback and recording of a single track
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        
+        playSound()
     }
     
-    
-
+    func playSound() {
+        // Bundle: Representation of Code and Resources, stored on a disk.
+        // Using Main bundle to locate the sound file resource
+        // url is looking for resources i.e. C.wav
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
